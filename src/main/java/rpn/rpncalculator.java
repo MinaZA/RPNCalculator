a9ed63bccd21971548e3a4db9d4752ffac728f2d
ZAKHARY Mina <85484181+MinaZA@users.noreply.github.com>
2024-06-26 15:20:03 +0200

    init_rpn_addition

---
+++ src/main/java/rpn/rpncalculator.java
@@ -0,0 +1,34 @@
+package rpn;
+
+import java.util.Stack;
+public class RPNCalculator {
+    public double evaluate(String expression) {
+        Stack<Double> stack = new Stack<>();
+        String[] tokens = expression.split("\\s+");
+
+        for (String token : tokens) {
+            switch (token) {
+                case "+":
+                    if (stack.size() < 2) throw new IllegalArgumentException("Invalid RPN expression");
+                    stack.push(stack.pop() + stack.pop());
+                    break;
+                default:
+                    try {
+                        stack.push(Double.parseDouble(token));
+                    } catch (NumberFormatException e) {
+                        throw new IllegalArgumentException("Invalid token: " + token);
+                    }
+            }
+        }
+
+        if (stack.size() != 1) throw new IllegalArgumentException("Invalid RPN expression");
+
+        return stack.pop();
+    }
+
+    public static void main(String[] args) {
+        RPNCalculator calculator = new RPNCalculator();
+        String expression = "3 4 +";
+        System.out.println("Result: " + calculator.evaluate(expression));
+    }
+}
